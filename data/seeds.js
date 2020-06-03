6a9034032ba08e46fccfb55f0e99afbba1a0104f
Piepongwong <j.tonneyck@gmail.com>
2020-06-03 11:24:23 +0200

    seed file working

---
+++ data/seeds.js
@@ -0,0 +1,43 @@
+const Movie = require("../models/Movie");
+const Director = require("../models/Director");
+const movies = require("./movies.json");
+const directors = require("./directors.json");
+
+moviesPromises = [];
+movies.forEach((movie)=> {
+    let director = directors.find((director)=> director.name === movie.director);
+    if(director){
+        moviesPromises.push(
+            Movie.create({
+                title: movie.title || "",
+                year: movie.year || "" ,
+                director: {
+                    name: director.name,
+                    image_url: director.image_url,
+                    img_url_url: director.image_url_url,
+                    biography: director.biography
+                },
+                duration: movie.duration,
+                genre: movie.genre || [],
+                rate: movie.rate || ""
+            })
+        )
+    }
+})
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/movies-embedded",{ 
+    useUnifiedTopology: true,
+    useNewUrlParser: true 
+})
+.then((connection)=> {
+    console.log("connected")
+    return Promise.all(moviesPromises);
+})
+.then((movies)=> {
+    console.log("Database Seeded", movies);
+    mongoose.connection.close();
+})
+.catch((err)=> {
+    console.log("Not seeded", err);
+})
+
